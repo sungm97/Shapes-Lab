385f7cd68950da3e3ea66c5b896b5903efd627f1
Seungmin Cho <smin8475@gmail.com>
2024-10-07 15:03:22 -0500

    Started Shape Polymorphism

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+class Shape
+{
+    public:
+        virtual double getArea() = 0;
+        virtual double getPerimeter() = 0;
+
+};
+class Rectangle : public Shape
+{
+    public:
+        double getArea() = 
+}
+
+int main()
+{
+    return 0;
+}
